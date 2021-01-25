28ab2416f799476763a014692f089d32ae160d8c
JoseanSG <jantoniosg98@gmail.com>
2021-01-25 00:05:16 -0600

    interfaz funcionando al 100, algoritmo no tanto xd

--- interfaz.py
+++ interfaz.py
@@ -1,15 +1,16 @@
 from tkinter import *
 from proyecto_automatas import *
 from tree_generator import *
+from PIL import ImageTk,Image
 
 class Interfaz:
     def __init__(self, ventana):
         #Inicializar la ventana con un título
         self.ventana=ventana
-        self.ventana.title("Calculadora")
+        self.ventana.title("Expresiones")
 
         #_______EXPRESIONES________
-        #Agregar una caja de texto para que sea la pantalla de la calculadora
+        #Agregar una caja de texto 
         self.textAsignacion=Text(self.ventana, state="normal", width=20, height=20, font=("Helvetica",15))
         #Label de asignaciones
         self.labelExp = Label(text = "Expresiones", font = ("Helvetica",15))
@@ -44,6 +45,17 @@ class Interfaz:
         #Ubicar la pantalla en la ventana
         self.pantalla.grid(row=1, column=8, columnspan=4, padx=5, pady=5)
 
+        #_______CUADRO DE IMAGEN PARA EL ARBOL_________
+        #creamos el cuadro de imagen
+        self.canvas = Canvas(self.ventana, width = 500, height = 500) 
+        self.img = ImageTk.PhotoImage(Image.open("graficas/nada.png"))  
+        self.canvas.create_image(50, 50, anchor=NW, image=self.img)
+        #agregamos la etiqueta
+        self.labelArbol = Label(text = "Arbol de expresion", font = ("Helvetica",15))
+        self.labelArbol.grid(row=0, column=16, columnspan=4, padx=5, pady=5)
+
+
+        self.canvas.grid(row=1, column=16, columnspan=4, padx=5, pady=5)
         #Crear boton de analizar
         botonAnalizar=Button(self.ventana, text="analizar", width=9, height=1, font=("Helvetica",15),command=self.analize)
         botonAnalizar.grid(row=6, column=0, columnspan=4, padx=5, pady=5)
@@ -51,19 +63,11 @@ class Interfaz:
         #Crear boton de analizar
         botonArbol=Button(self.ventana, text="Generar Arbol", width=15, height=1, font=("Helvetica",15),command=self.generateTree)
         botonArbol.grid(row=6, column=4, columnspan=4, padx=5, pady=5)
-        
 
-        # #Ubicar los botones con el gestor grid
-        # botones=[boton1, boton2, boton3, boton4, boton5, boton6, boton7, boton8, boton9, boton10, boton11, boton12, boton13, boton14, boton15, boton16, boton17]
-        # contador=0
-        # for fila in range(1,5):
-        #     for columna in range(4):
-        #         botones[contador].grid(row=fila,column=columna)
-        #         contador+=1
-        # #Ubicar el último botón al final
-        # botones[16].grid(row=5,column=0,columnspan=4)
+        #crear boton de limpiar
+        botonArbol=Button(self.ventana, text="Limpiar", width=15, height=1, font=("Helvetica",15),command=self.cleanAll)
+        botonArbol.grid(row=6, column=8, columnspan=4, padx=5, pady=5)
 
-        # return
 
     def analize(self):  
         expresion = self.textAsignacion.get(1.0, "end-1c")
@@ -101,6 +105,25 @@ class Interfaz:
                 tree_tokens_fixed.append(tok)
         print(tree_tokens_fixed)
         Tree.create_tree2(tree_tokens_fixed)
+        self.img = ImageTk.PhotoImage(Image.open("graficas/arbol.gv.png"))  
+        self.canvas.create_image(50, 50, anchor=NW, image=self.img)
+
+    def cleanAll(self):
+        #limpiamos escritura
+        self.textAsignacion.delete('1.0', END)
+        #limpiamos tabla de simbolos
+        self.pantalla.config(state=NORMAL)
+        self.pantalla.delete('1.0', END)
+        self.pantalla.config(state=DISABLED)
+        #limpiamos tabla de tokens
+        self.tokens.config(state=NORMAL)
+        self.tokens.delete('1.0', END)
+        self.tokens.config(state=DISABLED)
+        #limpiamos el diccionario de simbolos
+        SymbolTable.symbols = dict()
+        #limpiamos el arbol
+        self.img = ImageTk.PhotoImage(Image.open("graficas/nada.png"))  
+        self.canvas.create_image(50, 50, anchor=NW, image=self.img)
 
 ventana_principal=Tk()
 calculadora=Interfaz(ventana_principal)
--- tree_generator.py
+++ tree_generator.py
@@ -9,76 +9,6 @@ class Tree:
         return self.i - 1
 
     @staticmethod
-    def create_tree(tokens, i = i):    
-        dot = Digraph(comment='Arbol de expresion')
-
-        def create_hijo(i, npadre):
-            padre = npadre
-            hijo1 = 0
-            hijo2 = 0
-            while 0 < len(tokens):
-                token = tokens[-1]
-                if token == "+" or token == "-" or token == "*" or token == "/":
-                    if padre == 0:
-                        dot.node(str(i), token)
-                        tokens.pop()
-                        padre = i
-                        i += 1
-                        print("padre1")
-                    else:
-                        if hijo1 == 0:                        
-                            dot.node(str(i), token)
-                            hijo1 = i                            
-                            i = create_hijo(i, 0)# - 1
-                            hijo2 = i - 1                            
-                            # dot.edge(str(padre), str(hijo2))
-                            # dot.edge(str(padre), str(hijo1))                            
-                            #break
-                            #tokens.pop()
-                        else:
-                            dot.node(str(i), token)
-                            hijo2 = i
-                            create_hijo(i, 0)
-                            break
-                            #tokens.pop()
-                            print("padre2")
-                            #break
-                else:
-                    if hijo1 == 0:
-                        dot.node(str(i), token)
-                        tokens.pop()
-                        hijo1 = i
-                        i += 1
-                        print("padre3")
-                    else:
-                        if hijo2 == 0:
-                            print(hijo2)
-                            dot.node(str(i), token)
-                            hijo2 = i
-                            tokens.pop()
-                            i += 1
-                            break                                                                                                                                        
-                        else:
-                            if padre == 1:
-                                return i + 1
-                            else:                                    
-                                dot.node(str(i), token)
-                                hijo1 = padre
-                                padre -= 1
-                                hijo2 = i
-                                return i + 1
-                                break
-
-            dot.edge(str(padre), str(hijo2))
-            dot.edge(str(padre), str(hijo1))
-            return i   
-        create_hijo(i, 0)
-
-        print(dot.source)
-
-        dot.render('graficas/arbol.gv', view=False)
-#________________________________________________________________3
-    @staticmethod
     def create_tree2(tokens, i = i):    
         dot = Digraph(comment='Arbol de expresion')
 
@@ -88,59 +18,77 @@ class Tree:
             hijo2 = 0
             while 0 < len(tokens):
                 token = tokens[-1]
-                if token == "+" or token == "-" or token == "*" or token == "/":
+                print(token)
+                if token == "+" or token == "-" or token == "*" or token == "/":                    
                     if padre == 0:
+                        print("entro1")
                         dot.node(str(i), token)
                         padre = i
                         tokens.pop()
                         i +=1
+                        #si tiene un simbolo como hijo
                     elif hijo1 == 0:
+                        print("entro2")
                         hijo1 = i
+                        #lo almacena como hijo y lo pasa recursivamente para calcular sus propios hijos
                         dot.node(str(i), token)
-                        i = create_hijo(i, 0)  
+                        i = create_hijo(i, 0)  #
                         token = tokens[-1]
                         hijo2 = i
                         dot.node(str(i), token)                                       
                         tokens.pop()                        
                         i +=1
-                        break
+                        #para que la funcion no acabe si es que quedan mas elementos en la pila
+                        if len(tokens) > 0:
+                            dot.edge(str(padre), str(hijo2), rankdir="RL")
+                            dot.edge(str(padre), str(hijo1), rankdir="RL")                            
+                            padre = hijo2
+                            hijo1 = 0
+                            hijo2 = 0
+                            #regresa al while
+                        else:   
+                            #si ya no hay mas elementos acaba la ejecucion                         
+                            break
+                        #REVISAR EL CASO CUANDO HIJO 1 E HIJO 2 NO SON IGUALES A CERO
                     elif hijo2 == 0:
+                        print("entro3")
                         hijo2 = i
                         dot.node(str(i), token)
                         i = create_hijo(i, 0)
                         break
+                #REVISAR EL DESMADER CON EL DEBUGGER
                 else:
                     if hijo1 == 0:
+                        print("entro4")
                         dot.node(str(i), token)
                         hijo1 = i
                         tokens.pop()
                         i +=1
                     elif hijo2 == 0:
+                        print("entro5")
                         dot.node(str(i), token)
                         hijo2 = i
-                        tokens.pop()
                         i +=1
-                        break
-            dot.edge(str(padre), str(hijo1))
-            dot.edge(str(padre), str(hijo2))                        
+                        if len(tokens) > 1:
+                            print("entro6")
+                            tokens.pop()
+                            break
+                                                        
+                        else:
+                            print("entro7")
+                            tokens.pop()      
+                    # else:
+                    #     print("entro6")
+                    #     dot.edge(str(padre), str(hijo1))
+                    #     dot.edge(str(padre), str(hijo2))                        
+                    #     padre = hijo2
+                    #     hijo1 = 0
+                    #     hijo2 = 0
+            dot.edge(str(padre), str(hijo2), rankdir="RL")
+            dot.edge(str(padre), str(hijo1), rankdir="RL")                                   
             return i
         create_hijo(i, 0)
 
         print(dot.source)
-
+        dot.format = "png"
         dot.render('graficas/arbol.gv', view=False)      
-
-#HACER UNA FUNCION QUE ME VAYA GENERANDO LOS INDICES AUTOMATICAMENTE
-#AVERIGUAR COMO PODER HACER LAS RELACIONES CON LOS INDICES GENERAOS
-
-# dot.node('A', 'King Arthur')
-    # dot.node('B', 'Sir Bedevere the Wise')
-    # dot.node('L', 'Sir Lancelot the Brave')
-
-    # dot.edges(['AB', 'AL'])
-    # dot.edge('B', 'L', constraint='false')  
-    # 
-
-# pages = convert_from_path('graficas/arbol.pdf', 500)
-# for page in pages:
-#     page.save('graficas/out.png', 'PNG')
